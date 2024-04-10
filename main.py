4dc6bc44bfc5e0e4e7d7fb77e736d1e4bba74336
TobiW03 <162978803+TobiW03@users.noreply.github.com>
2024-04-10 12:48:16 +0200

    Update main.py
    updated version with user inputs

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from my_functions import estimate_max_hr, build_person, build_experiment
 #main
 if __name__ == "__main__":
-    #MaxHR
-    print("1. Proband")
-    print(estimate_max_hr(25,"male"))
-    print("2. Probandin")
-    print(estimate_max_hr(47,"female"))
-    #BuildPerson
-    print(build_person('Tobias','Wannenmacher','male',21))
-    #BuildExperiment
-    print(build_experiment('MGST23','2024-04-03','TW','MGST'))
+    #Userinputs
+    firstname = str(input('Please enter your firstname: '))
+    lastname = str(input('Please enter your lastname: '))
+    sex = str(input('Please enter your gender as male or female: '))
+    age = int(input('Please enter your age: '))
+    #create person
+    person1 = build_person(firstname,lastname,sex,age)
+    print(person1)
+    #maxHR
+    person1_maxHR = estimate_max_hr(age,sex)
+    print(person1_maxHR)
+    #build experiment
+    experimentname = str(input('Please enter the name of the experiment: '))
+    date = str(input('Please enter the current date: '))
+    supervisor = str(input('Please enter the name of the supervisor: '))
+    subject = str(input('Please enter the name of the subject of the experiment: '))
+    #creation of experiment
+    experiment1 = build_experiment(experimentname,date,supervisor,subject)
