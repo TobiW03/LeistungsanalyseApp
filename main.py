5815372eebea268ee322114a7cb8d305af3afa03
TobiW03 <162978803+TobiW03@users.noreply.github.com>
2024-04-18 16:33:31 +0200

    zurücksetzen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json 
-import my_classes
+from my_functions import estimate_max_hr, build_person, build_experiment
 #main
 if __name__ == "__main__":
     #Userinputs
@@ -7,17 +7,21 @@ if __name__ == "__main__":
     lastname = str(input('Please enter your lastname: '))
     sex = str(input('Please enter your gender as male or female: '))
     age = int(input('Please enter your age: '))
+    #create person
+    person1 = build_person(firstname,lastname,sex,age)
+    print(person1)
+    #maxHR
+    person1_maxHR = estimate_max_hr(age,sex)
+    print(person1_maxHR)
     #build experiment
     experimentname = str(input('Please enter the name of the experiment: '))
     date = str(input('Please enter the current date: '))
     supervisor = str(input('Please enter the name of the supervisor: '))
     subject = str(input('Please enter the name of the subject of the experiment: '))
+    #creation of experiment
+    experiment1 = build_experiment(experimentname,date,supervisor,subject)
 
-
-    #Objekterstellung
-    Person1 = my_classes.Person(firstname,lastname,sex,age) 
-    Experiment1 = my_classes.Experiment(experimentname,date,supervisor,subject)
-    print(Person1.__dict__)
-    print(Experiment1.__dict__)
-    my_classes.Person.save(Person1)
-    my_classes.Experiment.save(Experiment1)
+# Convert and write JSON object to file
+with open("sample.json", "a") as outfile: 
+    json.dump(experiment1, outfile)
+    json.dump(person1, outfile)
