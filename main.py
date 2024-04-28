504d6699b91b8bfdf906a8164b69829635c3b46c
TobiW3 <wt1651@mci4me.at>
2024-04-28 14:06:16 +0200

    Aufgabe 6.2

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import my_classes
 #main
 if __name__ == "__main__":
     #Userinputs
+    print("Inputs for subject: ")
     firstname = str(input('Please enter your firstname: '))
     lastname = str(input('Please enter your lastname: '))
     sex = str(input('Please enter your gender as male or female: '))
-    age = int(input('Please enter your age: '))
-    #build experiment
+    birthdate = str(input('Please enter your birthdate in the following style - dd.mm.yyyy: '))
+    #Supervisor
+    print("Inputs for supervisor: ")
+    firstnamesup = str(input('Please enter the firstname of the supervisor: '))
+    lastnamesup = str(input('Please enter the lastname of the supervisor: '))
+    ID = int(input('Please enter the ID of the supervisor: '))
+    #Experiment
+    print("Inputs for experiment: ")
     experimentname = str(input('Please enter the name of the experiment: '))
     date = str(input('Please enter the current date: '))
-    supervisor = str(input('Please enter the name of the supervisor: '))
-    subject = str(input('Please enter the name of the subject of the experiment: '))
-
-
     #Objekterstellung
-    Person1 = my_classes.Person(firstname,lastname,sex,age) 
-    Experiment1 = my_classes.Experiment(experimentname,date,supervisor,subject)
-    print(Person1.__dict__)
+    Subject1 = my_classes.Subject(firstname,lastname,sex,birthdate)
+    Supervisor1 = my_classes.Supervisor(firstnamesup,lastnamesup,ID)
+    Experiment1 = my_classes.Experiment(experimentname,date,Supervisor1,Subject1)
+    print(Subject1.__dict__)
+    print(Supervisor1.__dict__)
     print(Experiment1.__dict__)
-    my_classes.Person.save(Person1)
+    my_classes.Supervisor.save(Supervisor1)
+    my_classes.Subject.save(Subject1)
     my_classes.Experiment.save(Experiment1)
--- my_classes.py
+++ my_classes.py
@@ -1,26 +1,16 @@
 #Imports
 import json
+from datetime import datetime
 #Klassen
 class Person():
     """A simple attempt to model a person."""
-    def __init__(self, first_name,last_name,sex, age):
+    def __init__(self, first_name,last_name):
         """Initialize name and age attributes."""
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
-        self.estimated_max_hr = self.estimate_max_hr(sex, age)
-    def estimate_max_hr(self, sex,age_years):
-        if sex == "male":
-            max_hr_bpm =  223 - 0.9 * age_years
-        elif sex == "female":
-            max_hr_bpm = 226 - 1.0 *  age_years
-        else:
-            # der input() öffnet ein Eingabefenster für den Nutzer und speichert die Eingabe
-            max_hr_bpm  = input("Enter maximum heart rate:")
-        return int(max_hr_bpm)
     def save(self):
-        with open("sample.json", "a") as outfile: 
-            json.dump(self.__dict__, outfile)
+        with open("sample.json", "w") as outfile: 
+            json.dump(self.__dict__, outfile, indent = len(self.__dict__))
 
 class Experiment():
     """A simple attempt to model an experiment."""
@@ -28,8 +18,35 @@ class Experiment():
         """Initialize name and age attributes."""
         self.name = name
         self.date = date
-        self.supervisor = supervisor
-        self.subject = subject
+        self.supervisor = supervisor.__dict__
+        self.subject = subject.__dict__
     def save(self):
-        with open("sample.json", "a") as outfile: 
-            json.dump(self.__dict__, outfile)
+        with open("sample.json", "w") as outfile: 
+            json.dump(self.__dict__, outfile, indent = len(self.__dict__))
+
+class Subject(Person):
+    def __init__(self, first_name,last_name,sex, birthdate):
+        #Fkt super() -> Init aus Elternklasse
+        super().__init__(first_name, last_name)
+        self.__birthdate = birthdate
+        self.sex = sex
+        self.max_hr = self.estimate_max_hr(self.sex, self.calculate_age(birthdate))
+    def calculate_age(self, birthdate):
+        birthdate = datetime.strptime(birthdate, "%d.%m.%Y")
+        current_date = datetime.now()
+        age = current_date.year - birthdate.year - ((current_date.month, current_date.day) < (birthdate.month, birthdate.day))
+        return age
+    def estimate_max_hr(self, sex,age_years):
+        if sex == "male":
+            max_hr_bpm =  223 - 0.9 * age_years
+        elif sex == "female":
+           max_hr_bpm = 226 - 1.0 *  age_years
+        else:
+           max_hr_bpm  = input("Enter maximum heart rate:")
+        return int(max_hr_bpm)
+
+class Supervisor(Person):
+    def __init__(self, first_name,last_name, ID):
+        #Fkt super() -> Init aus Elternklasse
+        super().__init__(first_name, last_name)
+        self.ID = ID
