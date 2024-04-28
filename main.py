5d3bdbeb0c4c2e34e06aa1a920eab7bfbd2b716d
carinatilg <tc4640@mci4me.at>
2024-04-28 19:51:55 +0200

    Aufgabe 62

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-import json 
 import my_classes
 
-
-#main
 if __name__ == "__main__":
-    # Userinputs Person
-    firstname = str(input("Enter your firstname: ")) # "Carina"
-    lastname = str(input("Enter your lastname: ")) # "Tilg"
-    sex = str(input("Enter your gender [male/female]: ")) # "female"
-    age = int(input("Enter your age: ")) # 20
+    # Hardcoded inputs Person / Subject
+    subject_firstname = str(input("Enter the firstname of the subject: ")) # "Carina" # SubjectFN
+    subject_lastname = str(input("Enter the lastname of the subject: ")) # "Tilg" # SubjectLN
+    sex = str(input("Enter the gender of the subject: ")) # "female" # Subject sex
+    age = int(input("Enter the age of the subject: ")) #20 # Subject age
 
-    # Userinputs Experiment
-    experimentname = str(input("Enter the name of the experiment: ")) # "Experiment1"
-    date = str(input("Enter the current date [dd-mm-yyyy]: ")) # "20-04-2024"
-    supervisor = str(input("Enter the name of the supervisor: ")) # "Supervisor1"
-    subject = str(input("Enter the name of the subject of the experiment: ")) # "Subject1"
+    # Hardcoded inputs Experiment
+    experimentname = str(input("Enter the name of the experiment: ")) # "Experiment2024"
+    date = str(input("Enter the current date [dd-mm-yyyy]: ")) # "28-04-2024"
+    supervisor_first_name = str(input("Enter the firstname of the supervisor: ")) # "SupervisorFN"
+    supervisor_last_name = str(input("Enter the lastname of the supervisor: ")) # "SupervisorLN"
 
     # calling the class Person
-    person1 = my_classes.Person(firstname, lastname, sex, age)
+    person1 = my_classes.Person(subject_firstname, subject_lastname)
+
+    # calling the class Supervisor
+    supervisor1 = my_classes.Supervisor(supervisor_first_name, supervisor_last_name)
+
+    # calling the class Subject
+    subject1 = my_classes.Subject(subject_firstname, subject_lastname, sex, age)
     
     # calling the class Experiment
-    experiment1 = my_classes.Experiment(experimentname, date, supervisor, subject)
+    experiment1 = my_classes.Experiment(experimentname, date, supervisor1.__dict__, subject1.__dict__)
 
     # print dictionary in shell
     print(person1.__dict__)
@@ -29,3 +32,4 @@ if __name__ == "__main__":
     # save dictionaries as JSON
     my_classes.Person.save(person1)
     my_classes.Experiment.save(experiment1)
+
--- my_classes.py
+++ my_classes.py
@@ -2,14 +2,33 @@ import json
 
 # create a class Person
 class Person():
-    # constructor: with attributes of the parameters from my_function.py
-    def __init__(self, fist_name, last_name, sex, age):
+    # constructor
+    def __init__(self, fist_name, last_name):
         self.first_name = fist_name
-        self.last_name = last_name  
-        self.sex = sex
-        self.age = age
-        self.estimate_max_hr = self.estimate_max_hr(sex, age)
+        self.last_name = last_name
+
+    # method: save class attributes as a dictionary as JSON
+    def save(self):
+        # create a dictionary of the class attributes
+        with open("person.json", "a") as outfile: 
+            json.dump(self.__dict__, outfile)
+
+# create sub-class Supervisor of the class Person
+class Supervisor(Person):
+    # constructor
+    def __init__(self, fist_name, last_name):
+        super().__init__(fist_name, last_name)
     
+# create sub-class Subject of the class Person
+class Subject(Person):
+    # constructor
+    def __init__(self, fist_name, last_name, sex, age):
+        super().__init__(fist_name, last_name)
+        self.sex = sex
+        # hidden attribute: __age
+        self.__age = age
+        self.estimate_max_hr = self.estimate_max_hr(sex, age)
+
     # method: estimate the maximum heart rate
     def estimate_max_hr(self, sex, age_years):
         if sex == "male":
@@ -17,27 +36,20 @@ class Person():
         elif sex == "female":
             max_hr_bpm = 226 - 1.0 *  age_years
         else:
-            # der input() öffnet ein Eingabefenster für den Nutzer und speichert die Eingabe
             max_hr_bpm  = input("Enter maximum heart rate:")
         return int(max_hr_bpm)
-    
-    # method: save class attributes as a dictionary as JSON
-    def save(self):
-        # create a dictionary of the class attributes
-        with open("person.json", "a") as outfile: 
-            json.dump(self.__dict__, outfile)
-        
+
 # create a class Experiment
-class Experiment():
-    # constructor: with attributes of the parameters from my_function.py
+class Experiment(): 
+    # constructor
     def __init__(self, experiment_name, date, supervisor, subject):
         self.experiment_name = experiment_name
         self.date = date
         self.supervisor = supervisor
         self.subject = subject
-    
+
     # method: save class attributes as a dictionary as JSON
     def save(self):
         # create a dictionary of the class attributes
         with open("experiment.json", "a") as outfile: 
-            json.dump(self.__dict__, outfile)       
+            json.dump(self.__dict__, outfile)
