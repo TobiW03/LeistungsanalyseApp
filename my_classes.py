0fddc13390c06bb00485976ca7a5e69a23b1766c
carinatilg <tc4640@mci4me.at>
2024-05-05 22:21:01 +0200

    Aufgabe 8 unvollständig/fehlerhaft

--- my_classes.py
+++ my_classes.py
@@ -1,11 +1,19 @@
 import json
 
+import requests
+
 # create a class Person
 class Person():
     # constructor
     def __init__(self, fist_name, last_name):
         self.first_name = fist_name
         self.last_name = last_name
+
+    # methode to create a new person on the webserver
+    def put(self):
+        data = {'first_name': self.first_name}  # create a dictionary with the first name
+        response = requests.post('http://localhost:5000/person/', json=data) # send a POST request to the webserver
+        return response.json()
 
     # method: save class attributes as a dictionary as JSON
     def save(self):
@@ -22,12 +30,21 @@ class Supervisor(Person):
 # create sub-class Subject of the class Person
 class Subject(Person):
     # constructor
-    def __init__(self, fist_name, last_name, sex, age):
+    def __init__(self, fist_name, last_name, sex, age, email):
         super().__init__(fist_name, last_name)
         self.sex = sex
         # hidden attribute: __age
         self.__age = age
         self.estimate_max_hr = self.estimate_max_hr(sex, age)
+        self.email = email
+
+    def update_email(self):
+        data = {"name": self.first_name, "email": self.email}
+        
+        data_json = json.dumps(data)
+        response = requests.post("http://127.0.0.1:5000/person/Testname2", data=data_json)
+        return response.json()
+        
 
     # method: estimate the maximum heart rate
     def estimate_max_hr(self, sex, age_years):
